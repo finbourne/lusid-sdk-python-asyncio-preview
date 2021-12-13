7201fb806fb6acb4eba027fe8339863041bc725e
Rizwan Saeed <rizwan.saeed@finbourne.com>
2021-12-13 18:14:49 +0000

    Generate ids that can be disposed during test tear downs (#1)

--- sdk/tests/utilities/__init__.py
+++ sdk/tests/utilities/__init__.py
@@ -4,3 +4,4 @@ from tests.utilities.test_data_utilities import TestDataUtilities
 from tests.utilities.token_utilities import TokenUtilities
 from tests.utilities.temp_file_manager import TempFileManager
 from tests.utilities.mock_api_response import MockApiResponse
+from tests.utilities.id_generator import IdGenerator
